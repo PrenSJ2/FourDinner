325021eed41cb1c2357d8c555bee14bf435c14ee
Sebastian Prentice <prentice.seb@gmail.com>
2023-06-29 23:09:56 +0100

    Initial Commit

---
+++ FourDinner/FourDinnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FourDinnerApp.swift
+//  FourDinner
+//
+//  Created by Seb Prentice on 29/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FourDinnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FourDinnerUITests/FourDinnerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  FourDinnerUITestsLaunchTests.swift
+//  FourDinnerUITests
+//
+//  Created by Seb Prentice on 29/06/2023.
+//
+
+import XCTest
+
+final class FourDinnerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
